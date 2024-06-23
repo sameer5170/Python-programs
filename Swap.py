dfc98de31cfa7490461f73a71ddb4c44cafe9e00
sameer5170 <118465929+sameer5170@users.noreply.github.com>
2024-06-23 12:46:33 +0530

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,19 @@
+#num1=10
+#num2=20
+
+num1=input("enter a num1:")
+num2=input("enter a num2:")
+
+print("the no before swapping",num1)
+print("the no before swapping",num2)
+
+#Approach
+#temp=num1
+#num1=num2
+#num2=temp
+
+#Approach2
+num1,num2=num2,num1
+
+print("the no after swapping",num1)
+print("the no after swapping",num2)
